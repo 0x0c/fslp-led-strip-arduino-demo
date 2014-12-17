674f1cec68e8c1408656cb19e555c92c2c2a14df
Akira Matsuda <akira.matsuda@me.com>
2014-12-18 04:50:15 +0900

    UARTで送るための編集

--- FslpLedStripDemo/FslpLedStripDemo.ino
+++ FslpLedStripDemo/FslpLedStripDemo.ino
@@ -1,44 +1,3 @@
-/* FslpLedStripDemo: Example Arduino sketch that shows
- * how to use an Interlink force-sensing linear potentiometer
- * (FSLP) to control an Addressable RGB LED strip from Pololu.
- *
- * This code requires the PololuLedStrip library, which is
- * available from:
- *
- *   https://github.com/pololu/pololu-led-strip-arduino
- *
- * To use this code, you will need to plug an Addressable RGB LED
- * strip from Pololu into pin 12.
- *
- * You will need an additional resistor and four connections from
- * the FSLP to the Arduino.  The FSLP's integration guide from
- * Interlink can be used to make the correct connections as
- * follows:
- *
- * - Sense line (SL) to pin A2
- * - Drive line 1 (D1) to pin 8
- * - Drive line 1 (D2) to pin A3
- * - the connection made at the bottom of resistor R0 to pin 9
- *
- * After uploading the sketch, the Arduino will control the LED
- * strip using position and pressure readings from the FSLP.  The
- * position reading will determine the number of LEDs that light
- * up, while the pressure reading will determine the color of the
- * LEDs.
- *
- * This sketch also reports the force and pressure readings over
- * serial so you can read them on your computer using a terminal
- * program such as the Serial Monitor in the Arduino IDE.
- *
- * Without connecting an LED strip, this sketch can still be used
- * to take readings from the FSLP and see them on your computer.
- */
-
-// To measure position, the sense line must be connected to a
-// pin capable of reading analog voltages.  For pressure,
-// the sense line and drive line 2 must be connected to pins
-// capable of reading analog voltages.  See the FSLP guide for
-// more information.
 const int fslpSenseLine = A0;
 const int fslpDriveLine1 = 11;
 const int fslpDriveLine2 = A1;
@@ -47,109 +6,70 @@ const int fslpBotR0 = 10;
 void setup()
 {
   Serial.begin(9600);
+  pinMode(2,OUTPUT);
   delay(250);
 }
 
 void loop()
 {
   int pressure, position;
-
   pressure = fslpGetPressure();
-
-  if (pressure == 0)
-  {
-    // There is no detectable pressure, so measuring
-    // the position does not make sense.
+  if (pressure == 0) {
     position = 0;
   }
-  else
-  {
-    position = fslpGetPosition();  // Raw reading, from 0 to 1023.
+  else {
+    position = fslpGetPosition();
   }
-
-  char report[80];
-  sprintf(report, "pressure: %5d   position: %5d\n",
-    pressure, position);
-  Serial.print(report);
+  position /= 10;
+  
+  Serial.write(position & 0b01111111);
+  Serial.write(pressure | 0b10000000);
+//  char report[80];
+//  sprintf(report, "pressure: %5d   position: %5d\n", pressure, position);
+//  Serial.print(report);
+//  Serial.println(position & 0b01111111, BIN);
+//  Serial.println(pressure | 0b10000000, BIN);
 
   delay(20);
 }
 
-// This function follows the steps described in the FSLP
-// integration guide to measure the position of a force on the
-// sensor.  The return value of this function is proportional to
-// the physical distance from drive line 2, and it is between
-// 0 and 1023.  This function does not give meaningful results
-// if fslpGetPressure is returning 0.
 int fslpGetPosition()
 {
-  // Step 1 - Clear the charge on the sensor.
   pinMode(fslpSenseLine, OUTPUT);
   digitalWrite(fslpSenseLine, LOW);
-
   pinMode(fslpDriveLine1, OUTPUT);
   digitalWrite(fslpDriveLine1, LOW);
-
   pinMode(fslpDriveLine2, OUTPUT);
   digitalWrite(fslpDriveLine2, LOW);
-
   pinMode(fslpBotR0, OUTPUT);
   digitalWrite(fslpBotR0, LOW);
-
-  // Step 2 - Set up appropriate drive line voltages.
   digitalWrite(fslpDriveLine1, HIGH);
   pinMode(fslpBotR0, INPUT);
   pinMode(fslpSenseLine, INPUT);
-
-  // Step 3 - Wait for the voltage to stabilize.
   delayMicroseconds(10);
-
-  // Step 4 - Take the measurement.
   analogReset();
   return analogRead(fslpSenseLine);
 }
 
-// This function follows the steps described in the FSLP
-// integration guide to measure the pressure on the sensor.
-// The value returned is usually between 0 (no pressure)
-// and 500 (very high pressure), but could be as high as
-// 32736.
 int fslpGetPressure()
 {
-  // Step 1 - Set up the appropriate drive line voltages.
   pinMode(fslpDriveLine1, OUTPUT);
   digitalWrite(fslpDriveLine1, HIGH);
-
   pinMode(fslpBotR0, OUTPUT);
   digitalWrite(fslpBotR0, LOW);
-
   pinMode(fslpSenseLine, INPUT);
-
   pinMode(fslpDriveLine2, INPUT);
-
-  // Step 2 - Wait for the voltage to stabilize.
   delayMicroseconds(10);
-
-  // Step 3 - Take two measurements.
   analogReset();
   int v1 = analogRead(fslpDriveLine2);
   analogReset();
   int v2 = analogRead(fslpSenseLine);
-
-  // Step 4 - Calculate the pressure.
-  // Detailed information about this formula can be found in the
-  // FSLP Integration Guide.
-  if (v1 == v2)
-  {
-    // Avoid dividing by zero, and return maximum reading.
+  if (v1 == v2) {
     return 32 * 1023;
   }
   return 32 * v2 / (v1 - v2);
 }
 
-// Performs an ADC reading on the internal GND channel in order
-// to clear any voltage that might be leftover on the ADC.
-// Only works on AVR boards and silently fails on others.
 void analogReset()
 {
 #if defined(ADMUX)
